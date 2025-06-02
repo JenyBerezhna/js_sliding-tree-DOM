b2623e87a58767513658b7276306122b2508e1e1
Jeny Berezhna <JenySmails24@gmail.com>
2025-06-02 21:10:46 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  if (!tree) {
+    return;
+  }
+
+  tree.addEventListener('click', (eventy) => {
+    const header = eventy.target.closest('span');
+
+    if (header) {
+      const parentBlock = header.parentElement;
+      const children = parentBlock.querySelector('ul');
+
+      if (children) {
+        children.style.display =
+          children.style.display === 'none' ? 'block' : 'none';
+      }
+    }
+  });
+});
