b50ed9b319e21a7244d7026f78e7b587ec9c1754
Jeny Berezhna <JenySmails24@gmail.com>
2025-06-02 21:41:36 +0100

    make build

---
+++ index.a8ebe27e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".tree");e&&e.addEventListener("click",function(e){var t=e.target.closest("span");if(t){var n=t.parentElement.querySelector("ul");n&&(n.style.display="none"===n.style.display?"block":"none")}})});
+//# sourceMappingURL=index.a8ebe27e.js.map
